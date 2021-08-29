259608778e3f0fbf6cf82a5edd98d54e011833df
denisetra <denisetra@comcast.net>
2021-08-29 13:13:35 -0600

    Add files via upload

---
+++ Completed_Pong/frame.py
@@ -0,0 +1,52 @@
+from turtle import Turtle
+
+frame = Turtle()
+
+def outside_frame():
+    frame.penup()
+    frame.hideturtle()
+    frame.speed("fastest")
+    frame.color("yellow")
+    frame.goto(-395,290)
+    frame.pendown()
+    frame.setheading(0)
+    frame.forward(780)
+    frame.right(90)
+    frame.forward(780)
+    frame.right(90)
+    frame.forward(780)
+    frame.right(90)
+    frame.forward(780)
+
+    # middle line
+    frame.penup()
+    frame.goto(0,290)
+    frame.pendown()
+    frame.right(180)
+    frame.forward(580)
+
+    # frame.penup()
+    # frame.color("cyan")
+    # frame.goto(290,250)
+    # frame.shape("circle")
+    # frame.shapesize(.5,.5,1)
+    #
+    # frame.stamp()
+    # frame.goto(240,200)
+    # frame.write("(x=290,y=250)",move=False,align="left",font=("Arial",12,"normal"))
+    # frame.goto(-350,250)
+    # frame.stamp()
+    # frame.goto(-370,200)
+    # frame.write("(x-350,y=250)",move=False,align="Left",font=("Arial",12,"normal"))
+
+    # frame.goto(-350,-250)
+    # frame.stamp()
+    # frame.goto(-370,-220)
+    # frame.write("(x-350,y=-250)",move=False,align="Left",font=("Arial",12,"normal"))
+    #
+    # frame.goto(290,-250)
+    # frame.stamp()
+    # frame.goto(240,-220)
+    # frame.write("(x350,y=-250)",move=False,align="Left",font=("Arial",12,"normal"))
+
+
